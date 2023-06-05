d3f73cf26c9840b6e7fbfb45f0d7f7ef90d69054
raju1406 <122583391+raju1406@users.noreply.github.com>
2023-06-05 16:34:18 +0530

    06-array.sh

---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=("Apple" "Banana" "Orange")
+echo "FRUITS at 0th element ${FRUITS[0]}"
+echo "FRUITS at 1st element ${FRUITS[1]}"
+echo "FRUITS at 2nd element ${FRUITS[2]}"
+echo "FRUITS at all element ${FRUITS[@]}"
