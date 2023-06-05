7e3167427526dec847fb7a3869e6ff4dcbb31aae
raju1406 <122583391+raju1406@users.noreply.github.com>
2023-06-05 14:12:20 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NAME=$1
+WISH=$2
+echo "Hi ${NAME},Good ${WISH}"
+#how to know how many args passed they will ask interview
+echo "Numberof args passed are:$#"
+echo "Numberof args passed are:$@"
